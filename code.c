e351695e79141ea4e8e8f0cbb246972accbfdecf
HJJEON123 <58286836+HJJEON123@users.noreply.github.com>
2019-12-04 22:05:40 +0900

    Update code.c

--- code.c
+++ code.c
@@ -4,8 +4,8 @@ int main(void)
     int x=1;
     int y=2;
     
-    printf("1+2는 %d입니다. \n", 1+2);
-    printf("x+y는 %d입니다. \n", x+y);
+    printf("1+2는 %d입니다. \n", 1+2/*수식 1+2의 값을 출력한다.*/);
+    printf("x+y는 %d입니다. \n", x+y/*피연산자는 상수 또는 변수가 될 수 있다.*/);
     
     return 0;
 }
